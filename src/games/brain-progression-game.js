fe763bdffd4c4908be4d68d99db66119215b82de
roman-usov <66104246+roman-usov@users.noreply.github.com>
2023-01-11 18:39:43 +0400

    rectified lint error

--- src/games/brain-progression-game.js
+++ src/games/brain-progression-game.js
@@ -1,8 +1,7 @@
 import { generateRandomProgressionWithHiddenElement } from '../game-utils.js';
 
 export default function playProgressionGame(min, max) {
-  const { progression, hiddenNumber } =
-    generateRandomProgressionWithHiddenElement(min, max);
+  const { progression, hiddenNumber } = generateRandomProgressionWithHiddenElement(min, max);
 
   return {
     task: 'What number is missing in the progression?',
